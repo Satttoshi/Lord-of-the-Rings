cfd29e51b03f956346ca3fea026e9c75be3cc6b9
Josh <joshuastolle445@gmail.com>
2023-03-19 19:05:14 +0100

    small fixes

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,8 @@
 import DarkModeSwitch from "../components/dark-mode-switch";
 import GlobalStyle from "../styles";
+import { ThemeProvider } from "styled-components";
+
+import { useEffect } from "react";
 import useLocalStorageState from "use-local-storage-state";
 
 export default function App({ Component, pageProps }) {
@@ -7,15 +10,30 @@ export default function App({ Component, pageProps }) {
     defaultValue: false,
   });
 
+  console.log(darkMode);
+
   function handleDarkMode() {
     setDarkMode(!darkMode);
   }
 
   return (
-    <>
+    <ThemeProvider
+      theme={{
+        themeLight: {
+          color1: "#282828",
+          color2: "#f1f1f1",
+          color3: "#d4d1cd",
+        },
+        themeDark: {
+          color1: "#d4d1cd",
+          color2: "#282828",
+          color3: "#d4d1cd",
+        },
+      }}
+    >
       <GlobalStyle darkMode={darkMode} />
       <DarkModeSwitch onDarkMode={handleDarkMode} darkMode={darkMode} />
       <Component {...pageProps} />
-    </>
+    </ThemeProvider>
   );
 }
